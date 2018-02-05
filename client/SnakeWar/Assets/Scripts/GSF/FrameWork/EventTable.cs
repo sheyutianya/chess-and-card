8d956a872cccb8da78a2d6f669b79b712c8bfea9
sheyutianya <tianyasheren@Gmail.com>
2018-02-05 11:27:14 +0800

    aaa

---
+++ client/SnakeWar/Assets/Scripts/GSF/FrameWork/EventTable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Scripts.GSF.FrameWork
+{
+    public class ModuleEvent : UnityEvent<object>
+    {
+    }
+
+    public class ModuleEvent<T> : UnityEvent<T>
+    {
+    }
+
+    public class EventTable
+    {
+        private Dictionary<string, ModuleEvent> m_mapEvents;
+        public ModuleEvent GetEvent(string type)
+        {
+            if (m_mapEvents == null)
+            {
+                m_mapEvents = new Dictionary<string, ModuleEvent>();
+            }
+            if (!m_mapEvents.ContainsKey(type))
+            {
+                m_mapEvents.Add(type, new ModuleEvent());
+            }
+            return m_mapEvents[type];
+        }
+
+        public void Clear()
+        {
+            if (m_mapEvents != null)
+            {
+                foreach (var @event in m_mapEvents)
+                {
+                    @event.Value.RemoveAllListeners();
+                }
+                m_mapEvents.Clear();
+            }
+        }
+    }
+}
+
---
+++ client/SnakeWar/Assets/Scripts/GSF/FrameWork/Module.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OT.Foundation;
+
+namespace Scripts.GSF.FrameWork
+{
+    public abstract class Module
+    {
+        /// <summary>
+        /// 调用它以释放模块
+        /// </summary>
+        public virtual void Release()
+        {
+            this.Log("Release");
+        }
+    }
+}
